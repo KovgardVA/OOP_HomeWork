737e47b4c69c9b24a738d14bfc35fa0e7e2f82f0
AvraamOvolono <avraamovolono@yandex.ru>
2023-05-29 21:46:43 +0300

    Deleted old staff

--- Vending/Program.java
+++ Vending/Program.java
@@ -6,22 +6,17 @@ import java.util.List;
 public class Program {
     public static void main(String[] args) {
         BottleOfWater bottleOfWater1 = new BottleOfWater("Aqua", "Water co.", 50.99, 0.5);
-        BottleOfWater bottleOfWater2 = new BottleOfWater("Aqua Light", "Water co.", 55.99, 2);
-        BottleOfWater bottleOfWater3 = new BottleOfWater("Aqua Max", "Water co.", 59.99, 1.5);
-        BottleOfWater bottleOfWater4 = new BottleOfWater("Aqua Zero", "Water co.", 70.99, 1);
-        System.out.println(bottleOfWater1.displayInfo());
+        BottleOfWater bottleOfWater2 = new BottleOfWater("Aqua Sparkling", "Water co.", 105.99, 2);
 
         BottleOfMilk bottleOfMilk1 = new BottleOfMilk("Moo", "Cow co.", 62.99, 1.5, 2);
-        System.out.println(bottleOfMilk1.displayInfo());
 
         List<Product> products = new ArrayList<>();
         products.add(bottleOfWater1);
         products.add(bottleOfWater2);
         products.add(bottleOfMilk1);
-        products.add(bottleOfWater3);
-        products.add(bottleOfWater4);
 
         VendingMachine vendingMachine = new VendingMachine(products);
+
         BottleOfWater bottleOfWaterResult = vendingMachine.getBottleOfWater(2);
         if (bottleOfWaterResult != null) {
             System.out.println("Вы купили: ");
