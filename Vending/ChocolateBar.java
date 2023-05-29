ed8ac17a70ff7287cfcb88b53d767597f3ddf62b
AvraamOvolono <avraamovolono@yandex.ru>
2023-05-29 21:37:18 +0300

    Added chocolate type to Chocolate bar

--- Vending/ChocolateBar.java
+++ Vending/ChocolateBar.java
@@ -2,6 +2,7 @@ package Vending;
 
 public class ChocolateBar extends Product {
     private int weight;
+    private String chocolateType;
 
     public ChocolateBar(String name, String brand, double price, int weight) {
         super(name, brand, price);
@@ -16,8 +17,16 @@ public class ChocolateBar extends Product {
         this.weight = weight;
     }
 
+    public String getChocolateType() {
+        return chocolateType;
+    }
+    
+    public void setChocolateType(String chocolateType) {
+        this.chocolateType = chocolateType;
+    }
+
     @Override
     public String displayInfo() {
-        return String.format("[Шоколадный батончик] %s - %s - %f - [%d г.]", brand, name, price, weight);
+        return String.format("[Шоколадный батончик] %s - %s - %f - [%s шоколад - %d г.]", brand, name, price, weight);
     }
 }
