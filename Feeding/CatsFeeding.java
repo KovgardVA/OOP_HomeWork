232f3b403221c23781ce5111ace1878e52f35030
AvraamOvolono <avraamovolono@yandex.ru>
2023-06-01 18:57:13 +0300

    Main program draft

--- Feeding/CatsFeeding.java
+++ Feeding/CatsFeeding.java
@@ -2,6 +2,11 @@ package Feeding;
 
 public class CatsFeeding {
     public static void main(String[] args) {
+        Cat cat = new Cat("Барсик", 5);
+        Plate plate = new Plate(100);
         
+        plate.info();
+        cat.eat();
+        plate.setFood(plate.getFood() - cat.getAppetite());
     }
 }
