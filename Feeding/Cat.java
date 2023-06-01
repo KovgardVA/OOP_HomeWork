9fbfef4977e85cb0fea62b8ea92a1f0140dd8f66
AvraamOvolono <avraamovolono@yandex.ru>
2023-06-01 18:54:26 +0300

    Plate class added

--- Feeding/Cat.java
+++ Feeding/Cat.java
@@ -10,6 +10,22 @@ public class Cat {
         this.appetite = appetite;
     }
     
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public void setAppetite(int appetite) {
+        this.appetite = appetite;
+    }
+
     public void eat() {
         
     }
---
+++ Feeding/Plate.java
@@ -0,0 +1,23 @@
+package Feeding;
+
+public class Plate {
+    
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public void info() {
+        System.out.printf("В тарелке %d рыбок.");
+    }
+
+}
