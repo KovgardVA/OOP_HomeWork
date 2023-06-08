5eea206a41dc6f9e36b3c78c9132c39801fcfc50
AvraamOvolono <avraamovolono@yandex.ru>
2023-06-08 20:03:36 +0300

    Get box weight added.

--- Fruits/Box.java
+++ Fruits/Box.java
@@ -1,9 +1,38 @@
 package Fruits;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Box<T extends Fruit> {
     
+    private String name;
     private List<T> fruits;
+    private float weight;
+
+    public Box(String name) {
+        this.name = name;
+        fruits = new ArrayList<>();
+        weight = 0.0f;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void add(T fruit) {
+        fruits.add(fruit);
+    }
+
+    public float getWeight() {
+        for (T fruit : fruits) {
+            weight += fruit.getWeight();
+        }
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Box [name=" + name + ", fruits=" + fruits.size() + "]";
+    }
 
 }
--- Fruits/Program.java
+++ Fruits/Program.java
@@ -3,5 +3,14 @@ package Fruits;
 public class Program {
     public static void main(String[] args) {
         
+        Box<Orange> boxOrange1 = new Box<>("Бабушкины апельсины");
+        boxOrange1.add(new Orange());
+        boxOrange1.add(new Orange());
+
+        Box<Apple> boxApple1 = new Box<>("Дедушкины яблоки");
+        boxApple1.add(new Apple());
+
+        System.out.println(boxOrange1.getWeight());
+
     }
 }
